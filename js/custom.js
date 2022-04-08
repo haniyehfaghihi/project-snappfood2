32e3ac2fba029087b1cdf46f808d087bf12cc1c2
haniyehfaghihi <haniyeh.faghihi1999@yahoo.com>
2022-04-08 11:07:46 -0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,164 @@
+$(document).ready(function(){
+
+
+        function MyReload(){
+            location.reload();
+        }
+            
+        setInterval(MyReload, 62000);
+
+
+    
+        var Text = document.getElementById("Result");
+    
+        function firsttext() {
+            Text.innerHTML = "غذا";
+            setTimeout(secondtest, 2000);
+        }
+        function secondtest() {
+            Text.innerHTML = "خوار و بار";
+            setTimeout(thirdtest, 2000);
+        }
+        function thirdtest() {
+            Text.innerHTML = "نان";
+            setTimeout(Fourthtest, 2000);
+        }
+        function Fourthtest() {
+            Text.innerHTML = "میوه";
+            setTimeout(fifthtest, 2000);
+        }
+        function fifthtest() {
+            Text.innerHTML = "قهوه";
+            setTimeout(sixthtest, 2000);
+        }
+        function sixthtest() {
+            Text.innerHTML = "پیتزا";
+            setTimeout(Seventhtext, 2000);
+        }
+        function  Seventhtext() {
+            Text.innerHTML = "غذا";
+            setTimeout(eaghthtest, 2000);
+        }
+        function eaghthtest() {
+            Text.innerHTML = "خوار و بار";
+            setTimeout(ninethtext, 2000);
+        }
+        function ninethtext() {
+            Text.innerHTML = "نان";
+            setTimeout(tenthtext, 2000);
+        }
+        function tenthtext() {
+            Text.innerHTML = "میوه";
+            setTimeout(Eleventhtext, 2000);
+        }
+        function Eleventhtext() {
+            Text.innerHTML = "قهوه";
+            setTimeout(Twelfthtext, 2000);
+        }
+        function Twelfthtext() {
+            Text.innerHTML = "پیتزا";
+            setTimeout(Thirteenthtext, 2000);
+        }
+        function Thirteenthtext() {
+            Text.innerHTML = "غذا";
+            setTimeout(Fourteenthtext, 2000);
+        }
+        function Fourteenthtext() {
+            Text.innerHTML = "خوار و بار";
+            setTimeout(Fifteenthtest, 2000);
+        }
+        function Fifteenthtest() {
+            Text.innerHTML = "نان";
+            setTimeout(Sixteenthtest, 2000);
+        }
+        function Sixteenthtest() {
+            Text.innerHTML = "میوه";
+            setTimeout(Seventeenthtest, 2000);
+        }
+        function Seventeenthtest() {
+            Text.innerHTML = "قهوه";
+            setTimeout(Eighteenthtest, 2000);
+        }
+        function Eighteenthtest() {
+            Text.innerHTML = "پیتزا";
+            setTimeout(Nineteenthtest, 2000);
+        }
+        function Nineteenthtest() {
+            Text.innerHTML = "غذا";
+            setTimeout(Twentiethtest, 2000);
+        }
+        function Twentiethtest() {
+            Text.innerHTML = "خوار و بار";
+            setTimeout(Twentyfirsttest, 2000);
+        }
+        function Twentyfirsttest() {
+            Text.innerHTML = "نان";
+            setTimeout(Twentysecondtest, 2000);
+        }
+        function Twentysecondtest() {
+            Text.innerHTML = "میوه";
+            setTimeout(Twentythirdtest, 2000);
+        }
+        function Twentythirdtest() {
+            Text.innerHTML = "قهوه";
+            setTimeout(Twentyfourthtest, 2000);
+        }
+        function Twentyfourthtest() {
+            Text.innerHTML = "پیتزا";
+            setTimeout(Twentyfifthtest, 2000);
+        }
+        function Twentyfifthtest() {
+            Text.innerHTML = "غذا";
+            setTimeout(Twentysixthtest, 2000);
+        }
+        function Twentysixthtest() {
+            Text.innerHTML = "خوار و بار";
+            setTimeout(Twentyseventhtest, 2000);
+        }
+        function Twentyseventhtest() {
+            Text.innerHTML = "نان";
+            setTimeout(Twentyeighthtest, 2000);
+        }
+        function Twentyeighthtest() {
+            Text.innerHTML = "میوه";
+            setTimeout(Twentyninthtest, 2000);
+        }
+        function Twentyninthtest() {
+            Text.innerHTML = "قهوه";
+            setTimeout(Thirtytest, 2000);
+        }
+        function Thirtytest() {
+            Text.innerHTML = "پیتزا";
+        }
+
+        firsttext();
+
+
+
+
+
+    
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+});
+
